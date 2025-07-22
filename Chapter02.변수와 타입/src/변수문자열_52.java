1976f9d6cef34bf2e56058ffe6e968758b04c284
sist <sist@DESKTOP-Q88SLS4>
2025-07-22 17:58:03 +0900

    1111

---
+++ Chapter02.변수와 타입/src/변수문자열_52.java
@@ -0,0 +1,12 @@
+
+public class 변수문자열_52 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String news="앞서 양치승은 지난해 11월 채널A '절친 토큐멘터리-4인용 식탁'에 출연해 헬스장 보증금 사기 피해를 당했다고 고백했다.\r\n"
+				+ "\r\n"
+				+ "양치승은 \"전세 사기라는 것은 들어봤지만 내가 당할 거라 생각 못했다\"며 \"보증금을 내면 돌려주겠지 했는데, 그게 2년이 지나도 못 돌려받고 있다. 피해 금액만 5억원\"이라고 털어놨다.";
+		System.out.println(news);
+	}
+
+}
