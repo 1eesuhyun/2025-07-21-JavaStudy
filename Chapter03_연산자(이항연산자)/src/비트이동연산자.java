18168de4f33cdb65f0bc7b63a2fb95003b0f1484
sist <sist@DESKTOP-Q88SLS4>
2025-07-24 17:50:49 +0900

    1111

---
+++ Chapter03_연산자(이항연산자)/src/비트이동연산자.java
@@ -0,0 +1,18 @@
+// 비트 이동 연산자
+// << 왼쪽 , >> 오른쪽
+/*
+ *  4<<2 ==16
+ *  x<<y == x*2^y
+ *  4>>2 ==1
+ *  x>>y == x/2^y
+ */
+public class 비트이동연산자 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Integer.toBinaryString(27));
+		System.out.println(6<<2);
+		System.out.println(6>>2);
+	}
+
+}
