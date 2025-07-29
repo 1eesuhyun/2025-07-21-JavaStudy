094e5925a254259709dd8e165a60ad6a01e2d6c6
sist <sist@DESKTOP-Q88SLS4>
2025-07-30 08:53:39 +0900

    1111

---
+++ Chapter04_제어문(반복문)/src/제어문_1.java
@@ -0,0 +1,33 @@
+
+public class 제어문_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		for(int i=2;i<=10;i+=2)
+//		{
+//		System.out.println(i);
+//		}
+		
+//		for(int i=1;i==1;) 무한루프
+//		{
+//			System.out.println(i);
+//		}
+	}
+	/*
+	 *  알파벳 => 'A', 'a'...
+	 *  
+	 *  for(시작값;범위;증감식)
+	 *  {
+	 *  	반복 수행문
+	 *  }
+	 *  
+	 *  int 1=1 i<=5 i++
+	 *  System.out.println(i)
+	 *  
+	 *  반복문
+	 *    for      : 반복 횟수가 지정된 경우
+	 *    while    : 반복 횟수가 없는 경우 (선조건)
+	 *    do~while : 무조건 한번 이상 수행 (후조건)
+	 */
+
+}
